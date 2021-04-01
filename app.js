b95ca021df2e68326104f16816265c4dd8936c3b
Walter Huang <autumnhao@gmail.com>
2021-04-01 15:29:49 -0700

    Finish transferring the files to public folder.

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const app = express();
 
+
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.write(`<h1>This is my Newsletter!</h1>`);
-  res.send();
+ res.sendFile(__dirname + '/signup.html');
 });
 
 app.listen(3000, () => {
