214247b8494be425633857f13d0aebbbf9bd39e8
Walter Huang <autumnhao@gmail.com>
2021-04-02 12:21:07 -0700

    Finish adding the success and failure page to app.js.

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.post('/', (req, res) => {
   };
 
   const request = https.request(url, options, (response) => {
+
+    if (response.statusCode === 200) {
+      console.log(response);
+      res.sendFile(__dirname + '/success.html');
+    } else {
+      res.sendFile(__dirname + '/failure.html');
+    }
     response.on('data', (data) => {
       console.log(JSON.parse(data));
     });
